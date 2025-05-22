b6ff3b8e31aaf7043aa9846ee28a1fbbd01d72bf
ElboyevFirdavs <elboev.firdavs98@gmail.com>
2025-05-22 18:52:44 +0500

    Create lesson3.py

---
+++ lesson-3/homework/lesson3.py
@@ -0,0 +1,107 @@
+1.
+  fruits = ['apple', 'banana', 'cherry', 'mango', 'orange']
+  fruits[2] 
+
+2.
+  list1 = [1, 2, 3]
+  list2 = [4, 5, 6]
+  combined_list = list1 + list2
+  print("birlashtirilgan ro'yxat:", combined_list)
+
+3.
+  numbers = [10, 20, 30, 40, 50, 60, 70]
+  first = numbers[0]
+  middle_index = len(numbers) // 2
+  middle = numbers[middle_index]
+  last = numbers[-1]
+  new_list = [first, middle, last]
+  print("yangi ro'yxat:", new_list)
+
+4.
+  tupls = [1,2,3,4,5,6]
+  tupls
+
+5.
+  cities = ['London', 'New York', 'Tokyo', 'Paris', 'Berlin']
+  result = 'Paris' in cities
+  print(result
+
+6.
+  l1 = [1, 2, 3, 'a', 'b']
+  l2 = l1.copy()
+  l2
+
+7.
+  l1 = [1, 2, 3, 'a', 'b']
+  l2=l1*2
+
+
+
+my_list = [1, 2, 3, 4, 5]
+my_list[0], my_list[-1] = my_list[-1], my_list[0]
+print("List after swapping first and last elements:", my_list)
+
+8.
+numbers = tuple(range(1,11))
+slice_tuple=numbers[3:7]
+slice_tuple
+
+9.
+colors = ['blue','white', 'black', 'green', 'orange', 'blue',]
+colorss = colors.count('blue')
+colorss
+
+
+10.
+
+hayvonlar = ['snack', 'bear', 'monkey', 'lion']
+hayvon = hayvonlar.index('lion')
+hayvon
+
+11.
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+merged_tuple = tuple1 + tuple2
+print("Birlashtirilgan tuple:", merged_tuple)
+
+12.
+citiesss = ['London', 'New York', 'Tokyo', 'Paris', 'Berlin']
+citi = citiesss.__len__()
+citi
+13.
+
+my_tuple = (10, 20, 30, 40, 50)
+my_list = list(my_tuple)
+print("List:", my_list)
+
+14.
+
+numbers = (15, 42, 7, 23, 89, 4)
+max_value = max(numbers)
+min_value = min(numbers)
+print("Eng katta qiymat:", max_value)
+print("Eng kichik qiymat:", min_value)
+
+
+
+15.
+words = ('apple', 'banana', 'cherry', 'date', 'elderberry')
+reversed_words = words[::-1]
+print("Teskari tartibdagi tuple:", reversed_words)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
