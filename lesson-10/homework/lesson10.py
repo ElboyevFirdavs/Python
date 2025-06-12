1e23b941d07f867944c30e6bf2004ceb35fceaee
ElboyevFirdavs <elboev.firdavs98@gmail.com>
2025-06-12 09:44:36 +0500

    Create lesson10.py

---
+++ lesson-10/homework/lesson10.py
@@ -0,0 +1,26 @@
+1.
+Task klassi — sarlavha, tavsif, muddat va status saqlaydi.
+
+ToDoList klassi — task qo‘shish, belgilash, ko‘rsatish funksiyalariga ega.
+
+      todo_cli() — foydalanuvchi bilan oddiy muloqot menyusi.
+      todo_cli()
+2.
+   Homework 2: Simple Blog System
+Post klassi — sarlavha, kontent, muallif va vaqt saqlaydi.
+
+Blog klassi — post qo‘shish, ko‘rsatish, o‘chirish, tahrirlash imkoniyatlariga ega.
+
+blog_cli() — menyu orqali postlar bilan ishlash mumkin.
+
+Ishga tushirish uchun:
+
+      blog_cli()
+3.
+  Account klassi — hisob raqami, egasi va balansni saqlaydi.
+
+Bank klassi — hisob qo‘shish, balansni ko‘rish, pul yechish/yuklash, o‘tkazish.
+
+bank_cli() — bank amaliyotlarini CLI orqali boshqarish.
+
+        bank_cli()
